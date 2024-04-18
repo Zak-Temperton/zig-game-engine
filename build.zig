e3c5b2e27ce7860186820fdbe46e6ceb6a76e5fd
Zak-Temperton <zbtemperton@gmail.com>
2024-04-18 12:50:26 +1200

    add zmath and zstbi dependencies

--- build.zig
+++ build.zig
@@ -14,6 +14,8 @@ pub fn build(b: *std.Build) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
+    const zmath_dep = b.dependency("zmath", .{});
+    const zstbi_dep = b.dependency("zmath", .{});
 
     const lib = b.addStaticLibrary(.{
         .name = "game-engine",
@@ -23,6 +25,8 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
+    lib.root_module.addImport("zmath", zmath_dep.module("root"));
+    lib.root_module.addImport("zstbi", zstbi_dep.module("root"));
 
     // This declares intent for the library to be installed into the standard
     // location when the user invokes the "install" step (the default step when
